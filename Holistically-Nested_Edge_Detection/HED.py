58105f9030fddc8954137c8a79329cf5b1068883
qge2 <qge2@ncsu.edu>
2017-10-13 19:57:39 -0400

    change cross entropy input to sigmoid

--- Holistically-Nested_Edge_Detection/HED.py
+++ Holistically-Nested_Edge_Detection/HED.py
@@ -8,14 +8,86 @@ import tensorcv
 from tensorcv.models.layers import *
 from tensorcv.models.base import BaseModel
 from tensorcv.dataflow.image import *
+from tensorcv.utils.common import deconv_size
 
-def side_output(input_conv, o_height, o_width, name):
+def bilinear_upsample(input_im, up, out_shape = None):
+# https://github.com/BVLC/caffe/blob/master/include%2Fcaffe%2Ffiller.hpp#L244
+# https://github.com/ppwwyyxx/tensorpack/blob/master/tensorpack/models/pool.py#L145
+    def dconv_filter(filter_shape):
+        f = np.ceil(filter_shape) / 2.
+        c = (filter_shape - 1) / (2. * f)
+        ref = np.zeros((filter_shape, filter_shape), dtype='float32')
+        for x in range(0, filter_shape):
+            for y in range(0, filter_shape):
+                ref[x, y] = (1 - abs(x / f - c)) * (1 - abs(y / f - c))
+        return ref
+
+    with tf.name_scope('deter_bilinear_upsample'):
+        input_s = input_im.shape.as_list()
+
+        assert len(input_s) == 4, '[bilinear_upsample] Input tensor must be BHWC tensor!'
+        input_ch = input_s[-1]
+
+        up = int(up)
+        f_shape = up * 2
+        f_w = dconv_filter(f_shape)
+        f_w = np.repeat(f_w, input_ch * input_ch).reshape((f_shape, f_shape, input_ch, input_ch))
+        f_w = tf.constant(f_w, tf.float32,
+                                 shape=(f_shape, f_shape, input_ch, input_ch),
+                                 name='bilinear_upsample_filter')
+
+        in_shape = tf.shape(input_im)
+        if out_shape is None:
+            out_s = tf.stack([in_shape[0], tf.multiply(in_shape[1], up), 
+                            tf.multiply(in_shape[2], up), in_shape[3]])
+        else:
+            out_s = tf.stack([out_shape[0], out_shape[1], 
+                                  out_shape[2], out_shape[3]])
+
+        dconv = tf.nn.conv2d_transpose(input_im, f_w, 
+                                   output_shape=out_s, 
+                                   strides=[1, up, up, 1], 
+                                   padding='SAME', 
+                                   name='bilinear_upsample')
+        
+        # 
+        if out_shape is None:
+            if input_s[1]:
+                input_s[1] = input_s * up
+            if input_s[2]:
+                input_s[2] = input_s * up
+            dconv.set_shape(input_s)
+        else:
+            dconv.set_shape([None, None, None, input_ch])
+
+        return dconv
+
+def deconv_tensor_size(s, stride = 2):
+    stride_tensor = tf.constant(stride, tf.float32)
+    new_h = tf.cast(tf.ceil(tf.cast(s[1], tf.float32) / stride_tensor), tf.int32)
+    new_w = tf.cast(tf.ceil(tf.cast(s[2], tf.float32) / stride_tensor), tf.int32) 
+    return tf.stack([s[0], new_h, new_w, s[3]])
+           
+def side_output(input_conv, up, o_im, name):
     with tf.variable_scope(name) as scope:
-        side_out = conv(input_conv, 1, 1, 'conv', wd=0.0002,
+        side_out = conv(input_conv, 1, 1, 'conv', 
+                        wd=0.0002,
                         init_w=tf.constant_initializer(),
                         init_b=tf.constant_initializer())
-        side_out = tf.image.resize_bilinear(side_out, 
-                    [o_height, o_width], name='output')
+
+        if up >= 2:
+            o_shape = tf.shape(o_im)
+            out_shape = tf.stack([o_shape[0], o_shape[1], o_shape[2], 1])
+            shape_list = [out_shape,]
+            prev_s = out_shape
+            while up > 2:
+                prev_s = deconv_tensor_size(prev_s)
+                shape_list.append(prev_s)
+                up = up / 2
+
+            for o_s in shape_list[::-1]:
+                side_out = bilinear_upsample(side_out, 2, out_shape = o_s)
+
         return side_out
 
 def  class_balanced_cross_entropy_with_logits(logits, label, 
@@ -25,7 +97,10 @@ def  class_balanced_cross_entropy_with_logits(logits, label,
     '''
     with tf.name_scope(name) as scope:
         logits = tf.cast(logits, tf.float32)
+
+        
         label = tf.cast(label, tf.float32)
+
         num_pos = tf.reduce_sum(label)
         num_neg = tf.reduce_sum(1.0 - label)
 
@@ -93,6 +168,13 @@ class BaseHED(BaseModel):
 
     def _setup_summary(self):
         tf.summary.scalar("train_accuracy", self.accuracy, collections=['train'])
+        tf.summary.image('GT', tf.expand_dims(tf.cast(self.consensus_label, tf.float32), -1),
+                    collections = ['infer'])
+        with tf.name_scope('side_out'):
+            for idx, out in enumerate(self.output_list):
+                tf.summary.image("side_{}".format(idx), 
+                    tf.cast(tf.nn.sigmoid(out), tf.float32),
+                    collections = ['infer'])
 
     
 
@@ -144,35 +226,43 @@ class VGGHED(BaseHED):
             conv3_1 = conv(pool2, 3, 256, 'conv3_1')
             conv3_2 = conv(conv3_1, 3, 256, 'conv3_2')
             conv3_3 = conv(conv3_2, 3, 256, 'conv3_3')
-            conv3_4 = conv(conv3_3, 3, 256, 'conv3_4')
-            pool3 = max_pool(conv3_4, 'pool3', padding='SAME')
+            # conv3_4 = conv(conv3_3, 3, 256, 'conv3_4')
+            pool3 = max_pool(conv3_3, 'pool3', padding='SAME')
 
             conv4_1 = conv(pool3, 3, 512, 'conv4_1')
             conv4_2 = conv(conv4_1, 3, 512, 'conv4_2')
             conv4_3 = conv(conv4_2, 3, 512, 'conv4_3')
-            conv4_4 = conv(conv4_3, 3, 512, 'conv4_4')
-            pool4 = max_pool(conv4_4, 'pool4', padding='SAME')
+            # conv4_4 = conv(conv4_3, 3, 512, 'conv4_4')
+            pool4 = max_pool(conv4_3, 'pool4', padding='SAME')
 
             conv5_1 = conv(pool4, 3, 512, 'conv5_1')
             conv5_2 = conv(conv5_1, 3, 512, 'conv5_2')
             conv5_3 = conv(conv5_2, 3, 512, 'conv5_3')
-            conv5_4 = conv(conv5_3, 3, 512, 'conv5_4')
+            # conv5_4 = conv(conv5_3, 3, 512, 'conv5_4')
             # pool5 = max_pool(conv5_4, 'pool5', padding='SAME')
 
-        o_height, o_width = tf.shape(input_im)[1], tf.shape(input_im)[2]
+        shape_list = tf.stack([tf.shape(conv1_2), tf.shape(conv2_2), tf.shape(conv3_3), tf.shape(conv4_3), tf.shape(conv5_3)])
+        s = tf.identity(shape_list, name = 'check_shape')
 
-        side_1 = side_output(conv1_2, o_height, o_width, 'side_1')
-        side_2 = side_output(conv2_2, o_height, o_width, 'side_2')
-        side_3 = side_output(conv3_4, o_height, o_width, 'side_3')
-        side_4 = side_output(conv4_4, o_height, o_width, 'side_4')
-        side_5 = side_output(conv5_4, o_height, o_width, 'side_5')
+        o_height, o_width = tf.shape(input_im)[1], tf.shape(input_im)[2]
+        o_shape = tf.shape(input_im)
+        o_shape = tf.stack([o_shape[0], o_shape[1], o_shape[2], 1])
+
+        side_1 = side_output(conv1_2, 1, input_im, 'side_1')
+        side_2 = side_output(conv2_2, 2, input_im, 'side_2')
+        side_3 = side_output(conv3_3, 4, input_im, 'side_3')
+        side_4 = side_output(conv4_3, 8, input_im, 'side_4')
+        side_5 = side_output(conv5_3, 16, input_im, 'side_5')
+
+        
 
         with tf.variable_scope('output') as scope:
             side_mat = tf.concat([side_1, side_2, side_3, side_4, side_5], 3)
             self.output = conv(side_mat, 1, 1, 'fusion_weight',
                                 wd=0.0002, use_bias=False,
                                 init_w=tf.constant_initializer(0.2))
-            prediction = tf.squeeze(tf.greater(tf.nn.sigmoid(self.output), 0.5), axis = -1)
+            prediction_prob = tf.nn.sigmoid(self.output, name = 'pre_prob')
+            prediction = tf.squeeze(tf.greater(prediction_prob, 0.5), axis = -1)
             self.prediction = tf.cast(prediction, tf.int32, name='pre_label')
             self.output_list = [side_1, side_2, side_3, side_4, side_5, self.output]
 
@@ -180,8 +270,12 @@ class VGGHED(BaseHED):
         with tf.name_scope('loss'):
             # cost = []
             for idx, out in enumerate(self.output_list):
+                check_1 = tf.identity(tf.shape(out), name = 'out_shape')
+                check_2 = tf.identity(tf.shape(self.label), name = 'gt_shape')
+                out = tf.nn.sigmoid(out)
                 out = tf.squeeze(out, axis = -1)
-                side_cost = class_balanced_cross_entropy_with_logits(out, self.label, name = 'cost_{}'.format(idx))
+                self.consensus_label = tf.greater(self.label, 1)
+                side_cost = class_balanced_cross_entropy_with_logits(out, self.consensus_label, name = 'cost_{}'.format(idx))
                 tf.add_to_collection('losses', side_cost)
             
             return tf.add_n(tf.get_collection('losses'), name='result') 
--- Holistically-Nested_Edge_Detection/main.py
+++ Holistically-Nested_Edge_Detection/main.py
@@ -6,7 +6,7 @@ import argparse
 import tensorflow as tf
 
 import tensorcv
-from tensorcv.dataflow.dataset import BSDS500
+from tensorcv.dataflow.dataset import *
 from tensorcv.callbacks import *
 from tensorcv.train.config import TrainConfig
 from tensorcv.train.simple import SimpleFeedTrainer
@@ -16,23 +16,29 @@ from tensorcv.predicts import *
 from HED import VGGHED
 import config as config_path
 
-DATAPATH = 'E:\\GITHUB\\workspace\\CNN\\dataset\\BSR_bsds500\\BSR\\BSDS500\\data\\'
-VGGPATH = 'E:\\GITHUB\\workspace\\CNN\\pretrained\\vgg19.npy'
+
 
 def get_config(FLAGS):
     # data for training
+    # dataset_train = BSDS500HED('train',
+    #                         data_dir=config_path.data_dir_train)
     dataset_train = BSDS500('train',
-                            data_dir=config_path.data_dir)
+                            data_dir=config_path.data_dir_val)
 
     dataset_val = BSDS500('val',
-                          data_dir=config_path.data_dir)
+                          data_dir=config_path.data_dir_val)
+
+    dataset_test = BSDS500('infer',
+                          data_dir=config_path.data_dir_val)
 
 
     inference_list_validation = [
-                          InferScalars('accuracy/result', 'test_accuracy'),
+                          InferScalars(['accuracy/result', 'loss/result'], ['test_accuracy', 'loss']),
                     ]
     inference_list_test = [
-                          InferImages('output/pre_label', 'edge')
+                          InferImages('output/pre_prob', 'edge'),
+                          InferScalars('accuracy/result', 'test_accuracy_cat'),
+                          InferScalars('loss/result', 'loss_cat'),
                     ]
 
     return TrainConfig(
@@ -43,22 +49,24 @@ def get_config(FLAGS):
                  monitors = TFSummaryWriter(),
                  callbacks = [
                     ModelSaver(periodic=1000),
-                    TrainSummary(key='train', periodic=100),
-                    FeedInferenceBatch(dataset_val, 
-                                  periodic=50, 
-                                  batch_count=400, 
+                    TrainSummary(key=['train'], periodic=100),
+                    # FeedInferenceBatch(dataset_val, 
+                    #               periodic=100, 
+                    #               batch_count=400, 
+                    #               infer_batch_size = 1,
+                    #               inferencers=inference_list_validation),
+                    FeedInferenceBatch(dataset_test, 
+                                  periodic=1, 
+                                  batch_count=1, 
                                   infer_batch_size = 1,
-                                  inferencers=inference_list_validation),
-                    FeedInferenceBatch(dataset_val, 
-                                  periodic=100, 
-                                  batch_count=2, 
-                                  infer_batch_size = 1,
+                                  extra_cbs = TrainSummary(key = 'infer'),
                                   inferencers=inference_list_test),
-                    CheckScalar(['accuracy/result','loss/result'], 
-                                 periodic=10),
+                    CheckScalar(['accuracy/result','loss/result'], periodic=10),
+                    # CheckScalar(['check_shape'], periodic=1),
+                    # CheckScalar(['loss/out_shape', 'loss/gt_shape'], periodic=10),
                   ],
                  batch_size=1, 
-                 max_epoch=500,
+                 max_epoch=100,
                  summary_periodic=100,
                  default_dirs=config_path)
 
